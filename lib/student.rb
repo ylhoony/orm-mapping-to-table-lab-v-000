8ee42199ac25c16194e681475b3edf70fb3069e1
Hoon Lee <yl.hoony@gmail.com>
2017-12-29 18:13:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,6 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
 
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students").flatten[0]
-
   end
 
   def self.create(attributes)

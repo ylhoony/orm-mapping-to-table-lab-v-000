d80497afe7db4a77a93441e52cc23168ed847a4b
Hoon Lee <yl.hoony@gmail.com>
2017-12-29 18:02:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
 
   def self.create_table
     sql = <<- SQL
-      CREATE TABLE students (
+      CREATE TABLE IF NOT EXISTS students (
         id PRIMARY KEY INTEGER,
         name TEXT,
         grade INTEGER

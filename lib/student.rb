905fdfa8fdf313b705b82457b53c74a804acdc27
Hoon Lee <yl.hoony@gmail.com>
2017-12-29 18:11:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
-  attr_accessor :name, :grade
+  attr_accessor :name, :grade, :id
 
   def initialize(name, grade)
     @name = name

dab29e86d20fa12a687066c13ea1b167389f2fa3
Hoon Lee <yl.hoony@gmail.com>
2017-12-29 18:09:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,7 @@ class Student
       INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql, self.name, self.grade)
   end
 
   def self.create(attributes)
